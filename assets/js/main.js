a9bd9b978145a5c2162505f8e844685c9e501209
Francisco Contreras <66226740+franciscocontreras93@users.noreply.github.com>
2021-01-25 22:49:05 -0400

    asdasd

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+/*
+Template Name: Massive
+Author: GrayGrids
+*/
+
+(function () {
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+        
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+        var brand_logo = document.querySelector(".logo1")
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+            /* brand_logo.src = 'assets/images/logo/logo.svg'; */
+        } else {
+            header_navbar.classList.remove("sticky");
+            /* brand_logo.src = 'assets/images/logo/logo-white.svg'; */
+        }
+
+
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+    // for menu scroll 
+    var pageLink = document.querySelectorAll('.page-scroll');
+
+    pageLink.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                offsetTop: 1 - 60,
+            });
+        });
+    });
+
+    //===== close navbar-collapse when a  clicked
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    var navbarCollapse = document.querySelector(".collapse");
+
+    document.querySelectorAll(".page-scroll").forEach(e =>
+        e.addEventListener("click", () => {
+            navbarToggler.classList.remove("active");
+            navbarCollapse.classList.remove('show')
+        })
+    );
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    })
+    // WOW active
+    new WOW().init();
+
+    let filterButtons = document.querySelectorAll('.portfolio-btn-wrapper button');
+    filterButtons.forEach(e =>
+        e.addEventListener('click', () => {
+
+            let filterValue = event.target.getAttribute('data-filter');
+            iso.arrange({
+                filter: filterValue
+            });
+        })
+    );
+
+    var elements = document.getElementsByClassName("portfolio-btn");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+            var el = elements[0];
+            while (el) {
+                if (el.tagName === "BUTTON") {
+                    el.classList.remove("active");
+                }
+                el = el.nextSibling;
+            }
+            this.classList.add("active");
+        };
+    };
+
+    //====== counter up 
+    var cu = new counterUp({
+        start: 0,
+        duration: 2000,
+        intvalues: true,
+        interval: 100,
+        append: " ",
+    });
+    cu.start();
+})();
+
+const typed = new Typed('.typed', {
+	strings: [
+        /* '<i class="word-change">Sistemas de <br>Información <br>Geográfica</i>', */
+		'<i class="word-change">Geologia</i>',
+		'<i class="word-change">Geotecnia</i>',
+		'<i class="word-change">Geomarketing</i>',
+        '<i class="word-change">Catastro</i>', 
+        '<i class="word-change">Sensores Remotos</i>',       
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 85, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1500, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 85, // Velocidad en milisegundos para borrrar una letra,
+	//smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+GLightbox({
+            'href': 'https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM',
+            'type': 'video',
+            'source': 'youtube', //vimeo, youtube or local
+            'width': 900,
+            'autoplayVideos': true,
+        });
